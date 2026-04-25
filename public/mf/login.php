5196f13778b14082482831223f12516f59a8fe23
Matheus <matheusf296@gmail.com>
2026-04-25 19:43:38 -0300

    Adicionando paginas de testes

---
+++ public/mf/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste login</title>
+</head>
+<body>
+    <h1>Teste de Login</h1>
+    <form action="controller.php?id=2" method="post">
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" required><br><br>
+        
+        <label for="password">Senha:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        
+        <input type="submit" value="Entrar" name="submit" onclick="">
+</body>
+</html>
+
+
